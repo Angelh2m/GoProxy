50dad598e8aea4e65a97e385e015ebb8c5ade367
home <home@homes-MacBook-Pro.local>
2021-01-26 16:16:20 -0600

    Settings

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 Build for Amazon Linux
 
-GOOS=linux GOARCH=amd64 go build  proxy.go  
+``` GOOS=linux GOARCH=amd64 go build  proxy.go  ```
+
+### List Instances by public IP
+aws ec2 describe-instances \
+  --query "Reservations[*].Instances[*].PublicIpAddress"
---
+++ start.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+apt list --upgradable
+sudo apt-get update -y
+
+curl https://raw.githubusercontent.com/Angelh2m/GoProxy/master/proxy --output ~/home/ubuntu/.proxy
+sudo chmod 777 ~/home/ubuntu/.proxy
+
+sudo touch /etc/systemd/system/goProcess.service
+sudo chmod 777 /etc/systemd/system/goProcess.service
+
+cat <<EOT >> goProcess.service
+[Unit]
+Description=Go Server
+
+[Service]
+ExecStart=/home/ubuntu/.proxy
+WorkingDirectory=/home/ubuntu
+User=root
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+
+# <!-- systemctl settings -->
+
+# <!-- Add the service -->
+sudo systemctl enable goProcess.service
+
+# <!-- Start Service -->
+sudo systemctl start goProcess.service
+
+# <!-- Check if systemd started it. -->
+sudo systemctl status goProcess.service
+
+
+
+# <!-- https://gist.github.com/amazingandyyy/60edaede96fef0bc5524c01cb5a7f92b -->
